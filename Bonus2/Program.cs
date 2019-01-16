479a1aac3aa2d60355d7d633e1174aa1cf9dcf3c
mattlaura <lhuiefiction@gmail.com>
2019-01-15 19:44:43 -0500

    Initial commit for the bonus lab 2, calculating someone's age

---
+++ Bonus2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int year, month, day;
+            
+            Console.WriteLine("User what is your name?");
+            name = Console.ReadLine();
+
+
+            DateTime birthdate, today;
+            TimeSpan userAge;
+            int userDays, currentAge;
+            double Birthday;
+            Console.WriteLine(name + " when were you born?(enter MM/DD/YYYY)");
+            birthdate = Convert.ToDateTime(Console.ReadLine());
+
+            today = DateTime.Now;
+            userAge = today.Subtract(birthdate);
+            userDays = userAge.Days;
+            currentAge = (userDays / 365);
+            Console.WriteLine(name + " you are " + currentAge + " years old.");
+            Birthday = userAge.TotalDays;
+            if (birthdate.DayOfYear == today.DayOfYear)
+            {
+                Console.WriteLine("Happy Birthday today!");
+            }
+            
+                                 
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+            //figure out how to use the c# libary current date
+            //do a lay out similar to the C# libary current date to calucalte the age o the person
+            //wish them happy birthday if C# libary current date = date entered
+        }
+    }
+}
